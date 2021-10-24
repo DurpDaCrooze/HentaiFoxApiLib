90ff2a44de730b8ce5c2efb551724ca243e61e61
DurpDaCrooze <cassarluca@gmail.com>
2021-10-24 15:09:08 +0200

    Experimental pt.2

--- src/bedmcDB.java
+++ src/bedmcDB.java
@@ -1,2 +1,34 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class bedmcDB {
+
+    private static String ip = "jdbc:mysql://localhost:3306/bedmc";
+    private static String token = "root";
+
+    public static void set (Pool pool){
+        ip = pool.ip;
+        token = pool.token;
+    }
+
+    public static String sendData(String discorduser, String discordtag){
+        return connect(discorduser, discordtag);
+    }
+
+    private static String connect(String discordUser, String discordTag){
+        try{
+            Document connection = Jsoup.connect(ip + "/bedmc/database/inputuser").
+                    header("token", token).
+                    header("discordUser", discordUser).
+                    header("discordTag", discordTag).
+                    ignoreContentType(true).
+                    get();
+            return "Successfully inserted: " + discordUser + "#" + discordTag + " into: " + ip;
+        }catch (IOException e){
+            return e.toString();
+        }
+    }
+
 }
